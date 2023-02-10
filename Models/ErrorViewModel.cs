3e8760fa5032ccdc2b1ee37835a79fb61c5a156d
gze1206 <gze1206@naver.com>
2023-02-10 21:30:16 +0900

    First upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace blog.gze1206.net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
